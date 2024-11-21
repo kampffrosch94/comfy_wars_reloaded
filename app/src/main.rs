4b3c25d3bc2351112cdb73e0c7fee72437d8eb81
kampffrosch94 <kampffrosch94@github.com>
2024-11-21 09:20:51 +0100

    abstraction working

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,8 +1,13 @@
-use std::{path::Path, time::Instant};
+use std::{
+    path::{Path, PathBuf},
+    sync::mpsc::Receiver,
+    time::Instant,
+};
 
 use base::Context;
 
 use macroquad::prelude::*;
+use notify::{Event, INotifyWatcher, RecursiveMode, Watcher};
 
 #[macroquad::main("MyGame")]
 async fn main() {
@@ -19,58 +24,91 @@ async fn main() {
         time: Box::new(|| get_time()),
     };
 
+    let path = "../target/debug/libworker.so";
+    let mut worker = WorkerReloader::new(path);
+
     loop {
         clear_background(BLACK);
 
         let start = Instant::now();
-        call_dynamic(&mut ctx);
+        worker.update(&mut ctx);
         let duration = start.elapsed();
-
-        println!("Reload + Execution took: {:?}", duration);
+        // println!("Reload + Execution took: {:?}", duration)));
 
         //draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         //draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
 
-        let s = format!("FPS: {}", get_fps());
+        let fps = get_fps();
+        let s = format!("FPS: {}", if fps > 55 && fps < 65 { 60 } else { fps });
         draw_text(&s, 20.0, 20.0, 30.0, WHITE);
         next_frame().await
     }
 }
 
-fn call_dynamic(ctx: &mut Context) {
-    let path = "../target/debug/libworker.so";
-    if !Path::new(&path).exists() {
-        return;
-    }
-    unsafe {
-        let lib = libloading::Library::new(path).unwrap();
-        let symb: libloading::Symbol<extern "C" fn(&mut Context) -> ()> =
-            lib.get(b"update").unwrap();
-        let func: extern "C" fn(&mut Context) -> () = std::mem::transmute(symb.into_raw());
-        func(ctx);
-        lib.close().unwrap();
-    }
-}
-
 struct WorkerReloader {
-    worker: WorkerWrapper,
+    worker: Option<WorkerWrapper>,
+    receiver: Receiver<Result<Event, notify::Error>>,
+    path: PathBuf,
+    #[allow(unused)]
+    watcher: INotifyWatcher,
 }
 
 struct WorkerWrapper {
+    #[allow(unused)]
     lib: libloading::Library,
     update: extern "C" fn(&mut Context) -> (),
 }
 
 impl WorkerReloader {
     fn new(path: &str) -> Self {
-        let worker = unsafe {
+        let path = PathBuf::from(path);
+        let worker = Some(Self::create_worker(&path));
+
+        let (tx, receiver) = std::sync::mpsc::channel();
+
+        let mut watcher = notify::recommended_watcher(tx).unwrap();
+        watcher
+            .watch(path.parent().unwrap(), RecursiveMode::NonRecursive)
+            .unwrap();
+        Self {
+            worker,
+            watcher,
+            receiver,
+            path,
+        }
+    }
+
+    fn create_worker(path: &Path) -> WorkerWrapper {
+        unsafe {
             let lib = libloading::Library::new(path).unwrap();
             let symb: libloading::Symbol<extern "C" fn(&mut Context) -> ()> =
                 lib.get(b"update").unwrap();
             let update: extern "C" fn(&mut Context) -> () = std::mem::transmute(symb.into_raw());
             WorkerWrapper { lib, update }
-        };
+        }
+    }
 
-        Self { worker }
+    fn update(&mut self, ctx: &mut Context) {
+        let mut modified = false; // debounce reloading twice on multiple events
+        while let Ok(event) = self.receiver.try_recv() {
+            if let Ok(e) = event {
+                if e.kind.is_create()
+                    && e.paths
+                        .iter()
+                        .any(|p| p.file_name() == self.path.file_name())
+                {
+                    dbg!(&e);
+                    modified = true;
+                }
+            }
+        }
+
+        if modified && Path::new(&self.path).exists() {
+            self.worker = None; // need to unload before we can reload
+            println!("Reloading!");
+            self.worker = Some(Self::create_worker(&self.path));
+        }
+
+        (self.worker.as_ref().unwrap().update)(ctx);
     }
 }
--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -1,10 +1,7 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-
 use base::{Color, Context, Rect};
 
 #[no_mangle]
 pub extern "C" fn update(c: &mut Context) {
-    let start = SystemTime::now();
     (c.draw)(
         Rect {
             x: 80.,
@@ -14,15 +11,12 @@ pub extern "C" fn update(c: &mut Context) {
         },
         Color {
             r: 0.,
-            g: 1.,
+            g: 0.,
             b: 0.0,
             a: 1.0,
         },
     );
 
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
     let time = (c.time)();
     let dx = ( time * 3.).sin() * 200.;
     (c.draw)(
